2b66a283884dde472f22509fbfcd6fa66c76dd26
aadil-dev <adil62742@gmail.com>
2019-09-16 18:33:43 +0500

    Components exercise

---
+++ src/components/User.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm {{name}}</p>
+        <p>age:{{age}}</p>
+        <button @click="changeName">Change My Name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail 
+                :name="name" 
+                @nameWasReset="name = $event"
+                :resetFn="resetName"
+                :userAge='age'></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <app-user-edit 
+                :userAge="age"
+                @ageWasChanged="age=$event"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+        data: function(){
+            return {
+                    name: 'Aadil',
+                    age: 23
+                
+            };
+
+        },
+        methods:{
+            changeName(){
+                this.name='Muhammad Aadil';
+            },
+            resetName(){
+                this.name="Aadil";
+            }
+
+        },
+
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{name}}</p>
+        <p>User Age:{{userAge}}</p>
+        <button @click="resetFn()">Reset Name</button>
+    </div>
+</template>
+
+<script>
+export default{
+    props: {
+        name: String,
+        resetFn : Function,
+        userAge: Number 
+    },
+    methods:{
+        resetName(){
+            this.name='Adil Reset';
+            this.$emit('nameWasReset' , this.name);
+        }
+
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
